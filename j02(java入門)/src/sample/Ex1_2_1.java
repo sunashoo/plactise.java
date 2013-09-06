bbcd397d738afe6c4e7db28ea718aaba6d6ee964
git config --global user.name"shota" <git config --global user.email"sunashoo@gmail.com">
2013-09-07 01:23:25 +0900

    java入門学習用

---
+++ j02(java入門)/src/sample/Ex1_2_1.java
@@ -0,0 +1,33 @@
+package sample;
+	//<=コメント(改行ダメ）
+	/*
+	 ** コメント(改行できる)
+	 * 
+	 */
+
+public class Ex1_2_1 {
+	
+	/**
+	 * ドキュメーションコメント
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//改行して表示する
+		System.out.println("minus!");
+		System.out.println("zero!");
+		System.out.println("plus!");
+
+		System.out.print("Hello");
+		System.out.print("こんにちは");
+		//表示するだけで改行しない
+		System.out.println("");
+				
+		System.out.println("123+10は?");
+		System.out.println("----------");
+		System.out.println("133です");
+		System.out.println("正解です!");
+		
+		
+	}
+
+}
